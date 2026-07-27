f48e315a8cbb2fdf86dae500b214c2845cbf8789
edsonfsousa <edsonf.sousa0@gmail.com>
2026-07-27 18:59:44 -0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/corebank/apispringbootcorebank/ApiSpringbootCorebankApplication.java
@@ -0,0 +1,13 @@
+package com.corebank.apispringbootcorebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootCorebankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSpringbootCorebankApplication.class, args);
+    }
+
+}
